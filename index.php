fa895ad5dbeecf362af48e38e638a290e4e375e2
Khim070 <Vakhiml60@gmail.com>
2025-02-05 18:34:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <?php
         if(isset($_FILES['myfile']))
         {
+            $maxSize = 4 * 1024 * 1024;
+            $allowTypes = ['image/jpeg', 'image/png', 'image/jpg'];
             $fileCount = count($_FILES['myfile']['tmp_name']);
             if( $fileCount > 5){
                 echo "<p style='color:red;'>You can't upload more than 5 files</p>";
@@ -17,17 +19,24 @@
                     $tmp_name = $_FILES['myfile']['tmp_name'][$i];
                     $name = $_FILES['myfile']['name'][$i];
                     $size = $_FILES['myfile']['size'][$i];
-                    $maxSize = 4*1024*1024;
+                    $type = $_FILES['myfile']['type'][$i];
+
+                    if (!in_array($type, $allowTypes)){
+                        echo "<p style='color:red;'>$name is not an image. Only jpg, png, jpeg are allowed.</p>";
+                        continue;
+                    }
+
                     if($size > $maxSize){
                         echo "<p style='color:red;'>$name is too large. Maximum size is 4MB.</p>";
                         continue;
-                    }else{
-                        if (move_uploaded_file($tmp_name, "images/" . $name)) {
-                            echo "<p style='color:green;'>$name uploaded successfully.</p>";
-                        } else {
-                            echo "<p style='color:red;'>Failed to upload $name.</p>";
-                        }
                     }
+
+                    if (move_uploaded_file($tmp_name, "images/" . $name)) {
+                        echo "<p style='color:green;'>$name uploaded successfully.</p>";
+                    } else {
+                        echo "<p style='color:red;'>Failed to upload $name.</p>";
+                    }
+
                 }
             }
         }
